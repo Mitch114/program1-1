6041f179f781783127608ed1a839f0353b95a2ca
Mitchell Carr <77063411+Mitch114@users.noreply.github.com>
2022-01-20 21:27:09 -0500

    Add files via upload

---
+++ program1-1.js
@@ -0,0 +1,13 @@
+/* Find result for 4 * (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11) */
+
+let x = 1;
+let y = 3;
+let z = 4;
+let q = 5;
+let v = 7;
+let w = 9;
+let r = 11;
+
+n = z * (x- x/y + x/q - x/v + x/w - x/r)
+
+console.log(n)
